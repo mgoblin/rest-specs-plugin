71deb085ccd9e6bd7505acb4cc2e3e2c6d9704e0
mgoblin <mike.golovanov@gmail.com>
2020-07-02 14:33:55 +0300

    parse openApi spec for operation ids

---
+++ src/main/java/ru/uip/contract/OpenApiParser.java
@@ -0,0 +1,33 @@
+package ru.uip.contract;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OpenApiParser {
+
+    private final OpenAPI openAPI;
+
+    public OpenApiParser(String fileName, Project project) {
+        final File file = project.file(fileName);
+        if (!file.exists() || !file.canRead()) {
+            throw new IllegalArgumentException("OpenApi spec file " + fileName + " not found");
+        }
+        this.openAPI =  new OpenAPIV3Parser().read(fileName);
+    }
+
+    public List<String> parseOperationIds() {
+        return openAPI.getPaths().values().stream()
+                .flatMap(item -> item.readOperations().stream())
+                .map(Operation::getOperationId)
+                .collect(Collectors.toList());
+    }
+}
