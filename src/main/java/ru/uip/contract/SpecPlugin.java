676b47da2a3cb283b891bb05c8c3e4f2f449f519
mgoblin <mike.golovanov@gmail.com>
2020-07-02 19:28:32 +0300

    read contract file names by pattern. quick and dirty

--- src/main/java/ru/uip/contract/SpecPlugin.java
+++ src/main/java/ru/uip/contract/SpecPlugin.java
@@ -1,26 +1,36 @@
 package ru.uip.contract;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.PathItem;
-import io.swagger.v3.parser.OpenAPIV3Parser;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.Task;
 
-import java.io.File;
-import java.util.Collection;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SpecPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        final SpecPluginExtension ext = project.getExtensions().create("apiContractSpec", SpecPluginExtension.class);
+        final SpecPluginExtension apiExt = project.getExtensions().create("apiContractSpec", SpecPluginExtension.class);
+
         project.task("generate-api-spec").doLast(task -> {
-            final OpenApiParser openApiParser = new OpenApiParser(ext.getApiSpec(), project);
+            final OpenApiParser openApiParser = new OpenApiParser(apiExt.getApiSpec(), project);
             openApiParser.parseOperationIds().forEach(System.out::println);
+
+//            Path dir = FileSystems.getDefault().getPath("/home/mike/IdeaProjects/gradle_contracts/producer/src/test/resources/contracts");
+//            try(DirectoryStream<Path> stream = Files.newDirectoryStream( dir, "*.yml" )) {
+//                for (Path path : stream) {
+//                    System.out.println( path.getFileName() );
+//                }
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+
         });
+
     }
+
 }
--- src/main/java/ru/uip/contract/SpecPluginExtension.java
+++ src/main/java/ru/uip/contract/SpecPluginExtension.java
@@ -1,6 +1,11 @@
 package ru.uip.contract;
 
 import lombok.*;
+import org.gradle.api.file.ConfigurableFileCollection;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -9,4 +14,5 @@ import lombok.*;
 @NoArgsConstructor
 public class SpecPluginExtension {
     private String apiSpec = "src/main/api/api.yaml";
+    private Map<String, List<String>> operationContracts = new HashMap<>();
 }
