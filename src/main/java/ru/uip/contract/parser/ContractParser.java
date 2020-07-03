958f92eddd89b0b736c7dd6dd111249e2da57d1a
mgoblin <mike.golovanov@gmail.com>
2020-07-03 08:08:11 +0300

    parse contract yaml draft implementation

--- src/main/java/ru/uip/contract/parser/ContractParser.java
+++ src/main/java/ru/uip/contract/parser/ContractParser.java
@@ -1,7 +1,26 @@
 package ru.uip.contract.parser;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
+
+import java.io.IOException;
+import java.util.List;
+
 public class ContractParser {
-    public void parse(String fileContent) {
-        System.out.println(fileContent);
+
+    public void parse(String fileContent) throws IOException {
+        YAMLFactory yaml = new YAMLFactory();
+        ObjectMapper mapper = new ObjectMapper();
+
+        YAMLParser yamlParser = yaml.createParser(fileContent);
+        final List<ObjectNode> docs = mapper
+                .readValues(yamlParser, new TypeReference<ObjectNode>() {})
+                .readAll();
+        for(ObjectNode doc: docs) {
+            doc.fields().forEachRemaining(System.out::println);
+        }
     }
 }
