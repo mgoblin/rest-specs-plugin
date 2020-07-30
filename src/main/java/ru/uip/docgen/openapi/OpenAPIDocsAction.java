cd370c26627d4bfc5d41f1f543f34f21373f1665
mgoblin <mike.golovanov@gmail.com>
2020-07-30 20:27:56 +0300

    write spec to file

--- src/main/java/ru/uip/docgen/openapi/OpenAPIDocsAction.java
+++ src/main/java/ru/uip/docgen/openapi/OpenAPIDocsAction.java
@@ -9,8 +9,15 @@ import ru.uip.docgen.openapi.generator.spi.APISpecGenerator;
 import ru.uip.docgen.openapi.parser.OpenApiParser;
 import ru.uip.docgen.plugin.SpecPluginExtension;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.ServiceLoader;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @NonNullApi
 public class OpenAPIDocsAction implements Action<Task> {
@@ -37,6 +44,22 @@ public class OpenAPIDocsAction implements Action<Task> {
                 .findFirst().orElseThrow().get();
 
         logger.info("Find spec generator {}", apiSpecGenerator.getClass().getName());
+
+        final String specBody = apiSpecGenerator.generateSpec();
+        writeSpecToFile(apiExt.getOutputDir(), "testSpec.adoc", specBody);
+
+    }
+
+    public void writeSpecToFile(String outputDir, String fileName, String specBody) {
+        try {
+            Files.createDirectories(Paths.get(outputDir));
+            Path filePath = Paths.get(outputDir, fileName);
+
+            Files.deleteIfExists(filePath);
+            Files.write(filePath, specBody.getBytes(UTF_8), CREATE_NEW);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
